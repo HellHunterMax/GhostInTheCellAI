c09a430ca2866f306b704c5271494b3124cb3de1
Max <hellhunter15@hotmail.com>
2021-08-22 04:00:37 +0200

    Feaure: Basic AI, 1 command per turn.

--- GhostInTheCellAI/AI.cs
+++ GhostInTheCellAI/AI.cs
@@ -7,9 +7,12 @@ namespace GhostInTheCellAI
 {
     public class AI
     {
-        private List<Factory> Factories = new List<Factory>();
+        private readonly List<Factory> Factories = new List<Factory>();
         private string[] _Inputs;
         //TODO add remaining turns into calculation.
+        //TODO Add minuspoints for own owned factories.
+        //TODO push Cyborgs to closest to enemy.
+        //TODO Dont send more units then units in factory.
 
         public AI()
         {
@@ -59,45 +62,117 @@ namespace GhostInTheCellAI
 
         private string CalculateBestAction(List<Troop> troops)
         {
-            //TODO Find best action to take 1 step at a time.
-            //TODO create Points System
-            /*
-             * source troops - destination troops - TTA * production
-             * 60 3 10 * 3 = 141
-             * 60 10 2 * 1 = 48
-             * first step find closest takeover. base TTA.
-             * 
-             * (if enemy) source troops - (destination troops + (TTA * Produciton))
-             */
             //TODO add troops to possibletakeovers
-            //TODO step2 Choose best one.
+            //TODO Add remove 0 production factories from possibletakeovers except opponants.
             string action = "WAIT";
 
             List<Factory> ownedFactories = Factories.FindAll(fac => fac.Owner == Owner.Player);
-            Dictionary<Factory, List<Factory>> possibleTakeOvers = FindPossibleTakeOvers(ownedFactories);
+            Dictionary<Factory, Dictionary<Factory, int>> possibleTakeOvers = FindPossibleTakeOvers(ownedFactories);
             if (possibleTakeOvers.Count < 1)
             {
                 return action;
             }
 
+            Factory[] sourceAndDestination = FindSourceAndDestination(possibleTakeOvers);
+
+
+            //MOVE source destination cyborgCount
+            action = $"MOVE {sourceAndDestination[0].Id} {sourceAndDestination[1].Id} {sourceAndDestination[1].Cyborgs + 1}";
             return action;
         }
 
-        private static Dictionary<Factory, List<Factory>> FindPossibleTakeOvers(List<Factory> ownedFactories)
+        private static Factory[] FindSourceAndDestination(Dictionary<Factory, Dictionary<Factory, int>> possibleTakeOvers)
         {
-            Dictionary<Factory, List<Factory>> possibleTakeOvers = new Dictionary<Factory, List<Factory>>();
+            Factory source = null;
+            Factory destination = null;
+
+            KeyValuePair<Factory, int> factoryDestinationAndDistance = new KeyValuePair<Factory, int>();
+            foreach (var FactorySourceAndFactoryDest in possibleTakeOvers)
+            {
+                Factory newSource = FactorySourceAndFactoryDest.Key;
+                foreach (var newFactoryDestinationAndDistance in FactorySourceAndFactoryDest.Value)
+                {
+                    //Console.Error.WriteLine($"Current = {factoryDestinationAndDistance}");
+                    //Console.Error.WriteLine($"new = {newFactoryDestinationAndDistance.Key}");
+                    if (factoryDestinationAndDistance.Key == null)
+                    {
+                        source = FactorySourceAndFactoryDest.Key;
+                        factoryDestinationAndDistance = newFactoryDestinationAndDistance;
+                        destination = factoryDestinationAndDistance.Key;
+                    }
+                    else
+                    {
+                        if (!IsCurrentTargetBest(source.Cyborgs, newSource.Cyborgs, factoryDestinationAndDistance, newFactoryDestinationAndDistance))
+                        {
+                            source = newSource;
+                            factoryDestinationAndDistance = newFactoryDestinationAndDistance;
+                            destination = newFactoryDestinationAndDistance.Key;
+                        }
+                    }
+                }
+            }
+            Factory[] factories = { source, destination };
+            return factories;
+        }
+
+        private static bool IsCurrentTargetBest(int cyborgsCurrent, int cyborgsChallenger, KeyValuePair<Factory, int> CurrentFactoryAndDistance, KeyValuePair<Factory, int> ChallengerFactoryAndDistance)
+        {
+            bool currentIsClosest = CurrentFactoryAndDistance.Value <= ChallengerFactoryAndDistance.Value;
+
+            int cyborgsClose;
+            int cyborgsFurthest;
+            KeyValuePair<Factory, int> closestFactoryAndDistance;
+            KeyValuePair<Factory, int> FurthestFactoryAndDistance;
+
+            switch (currentIsClosest)
+            {
+                case true:
+                    cyborgsClose = cyborgsCurrent;
+                    cyborgsFurthest = cyborgsChallenger;
+                    closestFactoryAndDistance = CurrentFactoryAndDistance;
+                    FurthestFactoryAndDistance = ChallengerFactoryAndDistance;
+                    break;
+                case false:
+                    cyborgsClose = cyborgsChallenger;
+                    cyborgsFurthest = cyborgsCurrent;
+                    closestFactoryAndDistance = ChallengerFactoryAndDistance;
+                    FurthestFactoryAndDistance = CurrentFactoryAndDistance;
+                    break;
+            }
+
+
+            //closest	(cyborgs - dest + (abs(Sourcedistance - Destinationdistance)* production))* prod
+            //farthest  cyborgs - dest * prod
+            int score1 = (cyborgsClose - closestFactoryAndDistance.Key.Cyborgs + (Math.Abs(closestFactoryAndDistance.Value - FurthestFactoryAndDistance.Value)) * closestFactoryAndDistance.Key.Production) * closestFactoryAndDistance.Key.Production;
+            int score2 = (cyborgsFurthest - FurthestFactoryAndDistance.Key.Cyborgs) * FurthestFactoryAndDistance.Key.Production;
+
+            //Console.Error.Write($"currentIsClosest = {currentIsClosest}, score1 = {score1}, score2 = {score2}, ");
+            // Score 1 = (new) score 2 = (old)
+            //currentIsClosest = False, score1 = 30, score2 = 0, True
+            bool isScore1More = score1 >= score2;
+            bool answer = currentIsClosest ? isScore1More : !isScore1More;
+
+            Console.Error.WriteLine(answer);
+            return answer;
+        }
+
+        private static Dictionary<Factory, Dictionary<Factory, int>> FindPossibleTakeOvers(List<Factory> ownedFactories)
+        {
+            Dictionary<Factory, Dictionary<Factory, int>> possibleTakeOvers = new Dictionary<Factory, Dictionary<Factory, int>>();
             for (int i = 0; i < ownedFactories.Count; i++)
             {
                 Factory factory = ownedFactories[i];
                 int numberOfLinks = factory.Links.Count;
+                Console.Error.WriteLine($"Owned factory = {factory}");
 
-                List<Factory> possibleDestinations = new List<Factory>();
+                Dictionary<Factory, int> possibleDestinations = new Dictionary<Factory, int>();
                 for (int links = 0; links < numberOfLinks; links++)
                 {
                     Factory destinationFactory = GetDestinationFactory(factory, links);
                     if (factory.Cyborgs > destinationFactory.Cyborgs)
                     {
-                        possibleDestinations.Add(destinationFactory);
+                        possibleDestinations.Add(destinationFactory, factory.Links[links].Distance);
+                        Console.Error.WriteLine($"Possible Destination = {destinationFactory}");
                     }
                 }
                 if (possibleDestinations.Count > 0)
